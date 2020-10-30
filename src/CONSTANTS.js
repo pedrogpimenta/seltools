f6280d0360bb5d13be59a1851ee8fe0f3856eed2
pimentapedro <pedro@meetmaps.com>
2020-10-30 23:36:07 +0100

    Change Server variable

--- src/CONSTANTS.js
+++ src/CONSTANTS.js
@@ -1,1 +1,1 @@
-export const REACT_APP_SERVER_BASE_URL = process.env.REACT_APP_SERVER_BASE_URL || 'http://localhost:3000'
+export const REACT_APP_SERVER_BASE_URL = process.env.REACT_APP_SERVER_DEV_USERS_FOLDERS_URL || 'http://localhost:3000'
